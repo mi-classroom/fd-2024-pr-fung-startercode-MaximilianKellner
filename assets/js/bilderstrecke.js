e1dda864c6eca577710f7dba77a3cf08413253a4
MaximilianKellner <maximilian_elias.kellner@smail.th-koeln.de>
2024-09-26 13:31:01 +0200

    F6: Seite für eine abgeschlossene Arbeit erstellen (Basisversion)

---
+++ assets/js/bilderstrecke.js
@@ -0,0 +1,19 @@
+async function loadImagesFromJson() {
+    console.log('loadImagesFromJson');
+    const response = await fetch('./works/04-results/images/metadata.json');
+    const metadata = await response.json();
+    console.log(metadata);
+    const imageCarousel = document.querySelector('#image-carousel');
+    createImageItems(metadata, imageCarousel);
+}
+
+function createImageItems(images, container) {
+    const imagePath = '../works/n-pola/04-results/';
+    container.innerHTML = images.map(image => `
+        <li class="image-item">
+            <img src="${imagePath}${image.src}" alt="${image.metadata.Description || 'Bild'}" title="${image.metadata.Title}">
+        </li>
+    `).join('');
+}
+
+loadImagesFromJson();
